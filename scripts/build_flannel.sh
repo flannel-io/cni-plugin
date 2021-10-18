ba6878b22c1bf6c19498f5d803c50424271b1503
Manuel Buil <mbuil@suse.com>
2021-10-18 16:32:14 +0200

    Add release github action
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- scripts/build_flannel.sh
+++ scripts/build_flannel.sh
@@ -6,12 +6,12 @@ export GOOS="${GOOS:-linux}"
 export GOARCH="${GOARCH:-amd64}"
 export GOFLAGS="${GOFLAGS} -mod=vendor"
 
-mkdir -p "${PWD}/bin"
+mkdir -p "${PWD}/dist"
 
 echo "Building flannel for ${GOOS} in ${GOARCH}"
 
 if [ "$GOOS" == "linux" ]; then
-    go build -o "${PWD}/bin/flannel-${GOARCH}" "$@" .
+    go build -o "${PWD}/dist/flannel-${GOARCH}" "$@" .
 else
-    go build -o "${PWD}/bin/flannel.exe" "$@" .
+    go build -o "${PWD}/dist/flannel.exe" "$@" .
 fi
