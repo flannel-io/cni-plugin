355716c74fa0e105ccbf01575521df07f9cd8e86
Manuel Buil <mbuil@suse.com>
2021-10-26 12:42:34 +0200

    Add the version to the binary
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- scripts/build_flannel.sh
+++ scripts/build_flannel.sh
@@ -2,16 +2,28 @@
 set -e
 cd $(dirname "$0")/..
 
+if [ -z "$VERSION" ]; then
+	set +e
+	git describe --tags --abbrev=0 > /dev/null 2>&1
+	if [ "$?" != "0" ]; then
+		VERSION="master"
+	else
+		VERSION=$(git describe --tags --abbrev=0)
+	fi
+	set -e
+fi
+
 export GOOS="${GOOS:-linux}"
 export GOARCH="${GOARCH:-amd64}"
 export GOFLAGS="${GOFLAGS} -mod=vendor"
+export GLDFLAGS+="-X main.Version=${VERSION:-master}"
 
 mkdir -p "${PWD}/dist"
 
 echo "Building flannel for ${GOOS} in ${GOARCH}"
 
 if [ "$GOOS" == "linux" ]; then
-    go build -o "${PWD}/dist/flannel-${GOARCH}" "$@" .
+    go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${PWD}/dist/flannel-${GOARCH}" "$@" .
 else
-    go build -o "${PWD}/dist/flannel.exe" "$@" .
+    go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${PWD}/dist/flannel.exe" "$@" .
 fi
